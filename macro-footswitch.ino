51e2365b89efcbc299b2e8eed07ea3fc99bcfadf
Dan Matheson <dan@Dans-MacBook-Air.local>
2021-05-08 13:57:21 -0400

    lets try binary this time

--- macro-footswitch.ino
+++ macro-footswitch.ino
@@ -1,7 +1,7 @@
-/*********************************************************************
- * This is a 6 button BLE Macro Foot-Switch.                         * 
- * Button 1 sends the stored command from buttons 2-6 over Bluetooth.*
- * Just map the pins, set the macros, and go                         *
+/********************************************************************
+ * This is a 6 button BLE Macro Foot-Switch.                         *
+ * Button 1 sends the stored command from buttons 2-6 over Bluetooth. *
+ * Just map the pins, set the macros, and go                          *
  *                                                                   *
  * Changing the name of the device, manufaturer, and initial battery *
  * level can be done at "BleKeyboard bleKeyboard" below              *
@@ -11,8 +11,8 @@
  * https://github.com/T-vK/ESP32-BLE-Keyboard                        *
  ********************************************************************/
 #include <BleKeyboard.h>
-BleKeyboard bleKeyboard("Macro Foot-Switch", "Daaan", 69); 
-const int button0=36;    // set GPIO pins on ESP32. 
+BleKeyboard bleKeyboard("Macro Foot-Switch", "Daaan", 69);
+const int button0=36;    // set GPIO pins on ESP32.
 int buttonState0=0;     // Keep state to 0
 
 const int button1=39;
@@ -32,10 +32,10 @@ int buttonState5=0;
 
 void setup() {
   pinMode(button0, INPUT);
-  pinMode(button1, INPUT); 
-  pinMode(button2, INPUT); 
-  pinMode(button3, INPUT); 
-  pinMode(button4, INPUT);  
+  pinMode(button1, INPUT);
+  pinMode(button2, INPUT);
+  pinMode(button3, INPUT);
+  pinMode(button4, INPUT); 
   pinMode(button5, INPUT);
 
   Serial.begin(115200);
@@ -52,67 +52,46 @@ void macro_keys(uint8_t key_name){
   bleKeyboard.releaseAll();
   delay(1000);
 }
-void loop() {
-  buttonState0=digitalRead(button0);
-  buttonState1=digitalRead(button1);
-  buttonState2=digitalRead(button2);
-  buttonState3=digitalRead(button3);
-  buttonState4=digitalRead(button4);
-  buttonState5=digitalRead(button5);
 
-  //Macro-1
-  if(bleKeyboard.isConnected() 
-     && buttonState0 == HIGH 
-     && buttonState1 == HIGH 
-     && buttonState2 == LOW 
-     && buttonState3 == LOW 
-     && buttonState4 == LOW 
-     && buttonState5 == LOW) {
-    Serial.println("Sending Ctrl+Alt+Shift+F1...");
-    macro_keys(KEY_F1);
-  } 
-  //Macro-2
-  if(bleKeyboard.isConnected() 
-     && buttonState0 == HIGH 
-     && buttonState1 == LOW 
-     && buttonState2 == HIGH 
-     && buttonState3 == LOW 
-     && buttonState4 == LOW 
-     && buttonState5 == LOW) {
-    Serial.println("Sending Ctrl+Alt+Shift+F2...");
-    macro_keys(KEY_F2);
-  }
-  //Macro-3
-  if (bleKeyboard.isConnected() 
-      && buttonState0 == HIGH 
-      && buttonState1 == LOW 
-      && buttonState2 == LOW 
-      && buttonState3 == HIGH 
-      && buttonState4 == LOW 
-      && buttonState5 == LOW) {
-    Serial.println("Sending Ctrl+Alt+Shift+F3...");
-    macro_keys(KEY_F3);
-  }
-  //Macro-4
-  if (bleKeyboard.isConnected() 
-      && buttonState0 == HIGH 
-      && buttonState1 == LOW 
-      && buttonState2 == LOW 
-      && buttonState3 == LOW 
-      && buttonState4 == HIGH 
-      && buttonState5 == LOW) {
-    Serial.println("Sending Ctrl+Alt+Shift+F4...");
-    macro_keys(KEY_F4);
-  }
-  //Macro-5
-  if (bleKeyboard.isConnected() 
-      && buttonState0 == HIGH 
-      && buttonState1 == LOW 
-      && buttonState2 == LOW 
-      && buttonState3 == LOW 
-      && buttonState4 == LOW 
-      && buttonState5 == HIGH) {
-    Serial.println("Sending Ctrl+Alt+Shift+F5...");
-    macro_keys(KEY_F5);
-  }
+void loop() {
+
+  buttonState0=(digitalRead(button0)==HIGH)? 1:0;
+
+  buttonState1=(digitalRead(button1)==HIGH)? 1:0;
+  buttonState2=(digitalRead(button2)==HIGH)? 2:0;
+  buttonState3=(digitalRead(button3)==HIGH)? 4:0;
+  buttonState4=(digitalRead(button4)==HIGH)? 8:0;
+  buttonState5=(digitalRead(button5)==HIGH)? 16:0;
+  totalState=buttonState0*(buttonState1+buttonState2+buttonState3+buttonState4+buttonState5)
+
+    //Macro-1
+    if(bleKeyboard.isConnected()){
+      switch(totalState){
+        // Button 1 only
+      case 1:
+      Serial.println("Sending Ctrl+Alt_Shift+F1");
+      macro_keys(KEY_F1);
+      break;
+        // Button 2 only
+      case 2:
+      Serial.println("Sending Ctrl+Alt_Shift+F2");
+      macro_keys(KEY_F2);
+      break;
+        // Button 3 only
+      case 4:
+      Serial.println("Sending Ctrl+Alt_Shift+F3");
+      macro_keys(KEY_F3);
+      break;
+        // Button 4 only
+      case 8:
+      Serial.println("Sending Ctrl+Alt_Shift+F4");
+      macro_keys(KEY_F4);
+      break;
+        // Button 5 only
+      case 16:
+      Serial.println("Sending Ctrl+Alt_Shift+F5");
+      macro_keys(KEY_F5);
+      break;
+      }
+    }
 }
